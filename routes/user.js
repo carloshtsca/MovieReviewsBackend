98f25324677dd8926e2364d67927b4e314b7ab36
Carlos Henrique <chtsca2001@gmail.com>
2025-06-01 22:30:35 -0300

    Update methods

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const { create, verifyEmail, resendEmailVerificationToken, forgetPassword, sendResetPasswordTokenStatus, resetPassword, signIn } = require('../controllers/user');
 const { userValidator, validate, validatePassword, signInValidator } = require('../middlewares/validator');
 const { isValidPassResetToken } = require('../middlewares/user');
+const { isAuth } = require('../middlewares/auth');
 
 const router = express.Router();
 
@@ -12,5 +14,17 @@ router.post('/resend-email-verification-token', resendEmailVerificationToken);
 router.post('/forget-password', forgetPassword);
 router.post('/verify-pass-reset-token', isValidPassResetToken, sendResetPasswordTokenStatus);
 router.post('/reset-password', validatePassword, validate, isValidPassResetToken, resetPassword);
+
+router.get('/is-auth', isAuth, (req, res) => {
+    const { user } = req;
+    res.json({
+        user: {
+            id: user._id,
+            name: user.name,
+            email: user.email,
+            isVerified: user.isVerified
+        }
+    });
+});
 
 module.exports = router;
