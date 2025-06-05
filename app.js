c875e8dfda27a141e4e6fd9e07ecc0a0039392af
Carlos Henrique <chtsca2001@gmail.com>
2025-06-05 13:19:31 -0300

    Add Create Actor

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
-const userRouter = require('./routes/user');
 const { errorHandler } = require('./middlewares/error');
 const cors = require('cors');
 const { handleNotFound } = require('./utils/helper');
+
+const userRouter = require('./routes/user');
+const actorRouter = require('./routes/actor');
 
 require('dotenv').config();
 require('./db');
@@ -12,7 +14,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
+
 app.use('/api/user', userRouter);
+app.use('/api/actor', actorRouter);
 
 app.use(handleNotFound);
 
