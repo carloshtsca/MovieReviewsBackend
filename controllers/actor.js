8c5fe06f69b633cb5bee3f0d73d9ad284028c4e4
Carlos Henrique <chtsca2001@gmail.com>
2025-06-06 13:31:39 -0300

    Fetching Single Actor (Record)

--- controllers/actor.js
+++ controllers/actor.js
@@ -120,3 +120,13 @@ exports.getLatestActors = async (req, res) => {
     const result = await Actor.find().sort({ createdAt: -1 }).limit(12);
     res.json(result);
 }
+
+exports.getSingleActor = async (req, res) => {
+    const { id } = req.params;
+    if (!isValidObjectId(id)) return sendError(res, 'Invalid request!');
+
+    const actor = await Actor.findById(id);
+    if (!actor) return sendError(res, 'Invalid request, actor not found!', 404);
+
+    res.json(actor);
+}
