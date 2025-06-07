333002ca58691ad3ee9438110e28aab4c7615bb1
Carlos Henrique <chtsca2001@gmail.com>
2025-06-07 15:18:41 -0300

    isAdmin Middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -12,9 +12,17 @@ exports.isAuth = async (req, res, next) => {
     const { userId } = decode;
 
     const user = await User.findById(userId);
-    if (!user) return sendError(res, 'Invalid token user not found!', 404);
+    if (!user) return sendError(res, 'Unauthorized access!');
 
     req.user = user;
 
     next();
 };
+
+exports.isAdmin = (req, res, next) => {
+    const { user } = req;
+
+    if (user.role !== 'admin') return sendError(res, 'Unauthorized access!');
+
+    next();
+}
