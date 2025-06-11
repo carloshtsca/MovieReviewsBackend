abf93e3cef927c284d238775bf7ef59dda4592c4
Carlos Henrique <chtsca2001@gmail.com>
2025-06-11 17:17:29 -0300

    Storing Multiple Thumbnails

--- controllers/movie.js
+++ controllers/movie.js
@@ -1,7 +1,8 @@
 const cloudinary = require('../cloud');
 const storage_path = require('../cloud/path');
 const { sendError } = require('../utils/helper');
-
+const Movie = require('../models/movie');
+const { isValidObjectId } = require('mongoose');
 
 exports.uploadTrailer = async (req, res) => {
     const { file } = req;
@@ -22,6 +23,8 @@ exports.uploadTrailer = async (req, res) => {
 exports.createMovie = async (req, res) => {
     const { file, body } = req;
 
+    console.log(body);
+
     const {
         title,
         storyLine,
@@ -33,12 +36,70 @@ exports.createMovie = async (req, res) => {
         tags,
         cast,
         writers,
-        poster, 
-        trailer, 
+        trailer,
         language
     } = body;
 
-    console.log(req.body);
+    const newMovie = new Movie({
+        title,
+        storyLine,
+        releaseDate,
+        status,
+        type,
+        genres,
+        tags,
+        cast,
+        trailer,
+        language
+    });
 
-    res.send('ok');
+    if (director) {
+        if (!isValidObjectId(director)) return sendError(res, 'Invalid director id!');
+        newMovie.director = director;
+    }
+
+    if (writers) {
+        for (let writerId of writers) {
+            if (!isValidObjectId(writerId)) return sendError(res, 'Invalid writer id!');
+        }
+
+        newMovie.writers = writers;
+    }
+
+    // uploading poster
+    const { secure_url: url, public_id, responsive_breakpoints } = await cloudinary.uploader.upload(
+        file.path,
+        {
+            folder: storage_path('posters'),
+            transformation: {
+                width: 1280,
+                height: 720,
+            },
+            responsive_breakpoints: {
+                create_derived: true,
+                max_width: 640,
+                max_images: 3,
+            },
+        },
+    );
+
+    const finalPoster = { url, public_id, responsive: [] };
+
+    const { breakpoints } = responsive_breakpoints[0];
+
+    if (breakpoints.length) {
+        for (let imgObj of breakpoints) {
+            const { secure_url } = imgObj;
+            finalPoster.responsive.push(secure_url);
+        }
+    }
+
+    newMovie.poster = finalPoster;
+
+    await newMovie.save();
+
+    res.status(201).json({
+        id: newMovie._id,
+        title,
+    });
 };
--- models/movie.js
+++ models/movie.js
@@ -55,6 +55,7 @@ const movieSchema = mongoose.Schema({
         type: Object,
         url: { type: String, required: true },
         public_id: { type: String, required: true },
+        responsive: [URL],
         required: true,
     },
     trailer: {
