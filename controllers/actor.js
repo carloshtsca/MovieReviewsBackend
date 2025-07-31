2e29de715a1f91ab8b9b6ba7151ba038fa0dc286
Carlos Henrique <chtsca2001@gmail.com>
2025-07-31 10:50:52 -0300

    Little correction for search actor

--- controllers/actor.js
+++ controllers/actor.js
@@ -19,7 +19,7 @@ exports.createActor = async (req, res) => {
 
     await newActor.save();
 
-    res.status(201).json(formatActor(newActor));
+    res.status(201).json({ actor: formatActor(newActor) });
 }
 
 exports.updateActor = async (req, res) => {
@@ -85,7 +85,7 @@ exports.searchActor = async (req, res) => {
 
     const actors = result.map(actor => formatActor(actor));
 
-    res.json(actors);
+    res.json({ results: actors });
 }
 
 exports.getLatestActors = async (req, res) => {
