1937bc09efe763c2f319510ee27419748d44b06d
Carlos Henrique <chtsca2001@gmail.com>
2025-09-06 19:12:22 -0300

    Writing Logic For Pagination

--- routes/actor.js
+++ routes/actor.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { createActor, updateActor, removeActor, searchActor, getLatestActors, getSingleActor } = require('../controllers/actor');
+const { createActor, updateActor, removeActor, searchActor, getLatestActors, getSingleActor, getActors } = require('../controllers/actor');
 const { uploadImage } = require('../middlewares/multer');
 const { actorInfoValidator, validate } = require('../middlewares/validator');
 const { isAuth, isAdmin } = require('../middlewares/auth');
@@ -29,6 +29,7 @@ router.post(
 router.delete('/:actorId', isAuth, isAdmin, removeActor);
 router.get('/search', isAuth, isAdmin, searchActor);
 router.get('/latest-uploads', isAuth, isAdmin, getLatestActors);
+router.get('/actors', isAuth, isAdmin, getActors);
 router.get('/single/:id', getSingleActor);
 
 module.exports = router;
