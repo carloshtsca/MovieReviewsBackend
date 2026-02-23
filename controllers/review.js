69ffdbbef0e22296e5ca7ee64ba48400e5f480b4
Carlos Henrique <chtsca2001@gmail.com>
2026-02-23 19:42:49 -0300

    Removing Review

--- controllers/review.js
+++ controllers/review.js
@@ -54,3 +54,25 @@ exports.updateReview = async (req, res) => {
 
     res.json({ message: 'Your review has been updated.' });
 };
+
+exports.removeReview = async (req, res) => {
+    const { reviewId } = req.params;
+    const userId = req.user._id;
+
+    if (!isValidObjectId(reviewId)) return sendError(res, 'Invalid review ID!');
+
+    const review = await Review.findOne({ owner: userId, _id: reviewId });
+    if (!review) return sendError(res, 'Invalid request, review not found!');
+
+    // POrque não remove o id das reviews não funciona!
+    const movie = await Movie.findById(review.parentMovie).select('reviews');
+    movie.reviews = movie.reviews = movie.reviews.filter(
+        rId => rId.toString() !== reviewId.toString()
+    );
+
+    await Review.findByIdAndDelete(reviewId);
+
+    await movie.save();
+
+    res.json({ message: 'Review removed successfully.' });
+};
--- routes/review.js
+++ routes/review.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const { isAuth } = require('../middlewares/auth');
 const { validateRatings, validate } = require('../middlewares/validator');
-const { addReview, updateReview } = require('../controllers/review');
+const { addReview, updateReview, removeReview } = require('../controllers/review');
 
 router.post('/add/:movieId', isAuth, validateRatings, validate, addReview);
 router.patch('/:reviewId', isAuth, validateRatings, validate, updateReview);
+router.delete('/:reviewId', isAuth, removeReview);
 
 module.exports = router;
